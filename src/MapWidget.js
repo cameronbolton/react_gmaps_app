afa8003f7700821f3ffd8a2e24cb2b381c6f5ef3
cameronbolton <39877913+cameronbolton@users.noreply.github.com>
2023-01-07 17:15:04 -0600

    Minimal google maps implementation in react

---
+++ src/MapWidget.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useState, useCallback, useEffect } from 'react';
+
+import ScriptTag from 'react-script-tag';
+
+function MapWidget(){
+
+
+      // const [map, setMap] = useState(null)
+
+      // const initMap = useCallback(function callback() {
+      function initMap() {
+        console.log("in initMap")
+
+        let map = new window.google.maps.Map(document.getElementById("map"), {
+          zoom: 7,
+          //center: { lat: 30.4978559, lng: -97.7657881 }
+          center: { lng: 263, lat: 30 }
+          // center: { lat: -28, lng: 137 }
+        });
+        // NOTE: This uses cross-domain XHR, and may not work on older browsers.
+        map.data.loadGeoJson(
+          //"https://resume.daniellejwarden.com/data/Breweries.geojson"
+          //"https://storage.googleapis.com/mapsdevsite/json/google.json"
+          "Breweries.geojson"
+        );
+
+        // setMap(map)
+      // }, [])
+        }
+
+      // const initMap() {};
+      window.initMap = initMap;
+
+      useEffect (() => {
+        console.log('in useEffect')
+      }, [])
+      // useEffect (() => {
+      //   loadMap();
+      // }, [loadMap])
+
+      // const onUnmount = React.useCallback(function callback(map) {
+      //   setMap(null)
+      // }, [])
+    
+      return (
+        <div>
+          <ScriptTag isHydrating={true} type="text/javascript"
+            src= "https://maps.googleapis.com/maps/api/js?key=AIzaSyDRYLhxEC4AO-4No8hAmmbHSw6axTtrNxo&callback=initMap&v=weekly" 
+          />
+          Breweries:
+          <div id="map" style={{width: 800, height: 600}}></div>
+        </div>
+      )
+      
+}
+
+export default MapWidget;
